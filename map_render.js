eecf4d06e19a65c6aed85f0701e2c68cc56c4ca8
Muhammad Alam <alam@mmtutor.com>
2017-05-07 20:21:00 -0400

    style map and start working on button/modal

--- map_render.js
+++ map_render.js
@@ -19,19 +19,19 @@ window.ajax_success = function(data) {
       });
       switch ([repub,dem].toString()) {
         case '2,0':
-          return 'red';
+          return '#B24C63';
         case '1,1':
-          return 'purple';
+          return '#6E28A3';
         case '0,2':
-          return 'blue';
+          return '#2B5CCE';
         default:
-          return 'gray';
+          return '#40C471';
       }
     };
     state.color = partyColor(sens);
     state.style.fill = state.color;
   });
-}
+};
 
 var statesGeoJSON = 'https://d3js.org/us-10m.v1.json';
 
@@ -54,10 +54,11 @@ window.mmm = Array.from($(".states")[0].children);
 let tooltip = d3.select('body')
                     .append('div')
                     .style('position','absolute')
-                    .style('background','black')
+                    .style('background','#002b55')
                     .style('padding','5px 15px')
                     .style('border','1px #999999 solid')
-                    .style('opacity','0');
+                    .style('opacity','0')
+                    .style('border-radius', '5px');
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
@@ -100,24 +101,35 @@ const handleClick = (e) => {
   $('#twitter-timeline-container').append($(embedCode));
 
 };
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+
+
+
 
 const state_ids_obj = { 0: "AR", 1: "CA", 2: "IL", 3: "KS", 4: "MS", 5: "OH", 6: "TX", 7: "AL", 8: "IA", 9: "LA", 10: "MN", 11: "MO", 12: "NE", 13: "AZ", 14: "CO", 15: "IN", 16: "MI", 17: "MT", 18: "NY", 19: "OR", 20: "VA", 21: "WY", 22: "NC", 23: "OK", 24: "TN", 25: "WI", 26: "AK", 27: "VT", 28: "ND", 29: "GA", 30: "ME", 31: "RI", 32: "WV", 33: "ID", 34: "SD", 35: "NM", 36: "WA", 37: "PA", 38: "FL", 39: "UT", 40: "KY", 41: "NH", 42: "SC", 43: "NV", 44: "HI", 45: "NJ", 46: "CT", 47: "MD", 48: "MA", 49: "DE"};
 const state_hash = { "AL": "Alabama", "AK": "Alaska", "AS": "American Samoa", "AZ": "Arizona", "AR": "Arkansas", "CA": "California", "CO": "Colorado", "CT": "Connecticut", "DE": "Delaware", "DC": "District Of Columbia", "FM": "Federated States Of Micronesia", "FL": "Florida", "GA": "Georgia", "GU": "Guam", "HI": "Hawaii", "ID": "Idaho", "IL": "Illinois", "IN": "Indiana", "IA": "Iowa", "KS": "Kansas", "KY": "Kentucky", "LA": "Louisiana", "ME": "Maine", "MH": "Marshall Islands", "MD": "Maryland", "MA": "Massachusetts", "MI": "Michigan", "MN": "Minnesota", "MS": "Mississippi", "MO": "Missouri", "MT": "Montana", "NE": "Nebraska", "NV": "Nevada", "NH": "New Hampshire", "NJ": "New Jersey", "NM": "New Mexico", "NY": "New York", "NC": "North Carolina", "ND": "North Dakota", "MP": "Northern Mariana Islands", "OH": "Ohio", "OK": "Oklahoma", "OR": "Oregon", "PW": "Palau", "PA": "Pennsylvania", "PR": "Puerto Rico", "RI": "Rhode Island", "SC": "South Carolina", "SD": "South Dakota", "TN": "Tennessee", "TX": "Texas", "UT": "Utah", "VT": "Vermont", "VI": "Virgin Islands", "VA": "Virginia", "WA": "Washington", "WV": "West Virginia", "WI": "Wisconsin", "WY": "Wyoming" };
 
 const handleHoverOn = (e) => {
-  e.target.style.fill = 'yellow';
+  e.target.style.fill = '#FFFC61';
   tooltip.transition().style('opacity',1);
-      tooltip.html(e.target.state_abbr)
-      .style('left',(e.pageX)+'px')
+      tooltip.html(state_hash[e.target.state_abbr])
+      .style('left',(e.pageX - 50)+'px')
       .style('top',(e.pageY + 50)+'px');
 };
 
 const handleHoverOff = (e) => {
   e.target.style.fill = e.target.color;
+  tooltip.transition().style('opacity',0);
 };
 
 const handleFocus = (e) => {
-  e.target.style.fill = 'orange';
+  e.target.style.fill = '#C7C44B';
 };
 
 let iter = 0;
@@ -129,7 +141,7 @@ window.mmm.forEach( state => {
   state.onclick = handleClick;
   state.onmouseover = handleHoverOn;
   state.onmouseout = handleHoverOff;
-  state.onfocus = handleFocus;
+  // state.onfocus = handleFocus;
 
 } );
 svg.append("path")
